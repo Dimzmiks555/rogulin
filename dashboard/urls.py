b4a66910ae4f93140a11b7098a315aae003f4cc5
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-12-24 10:48:52 +0300

    'expenses_report'

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="dashboard"),
+    path('report/', views.report, name="report"),
+    path('report_expenses/', views.report_expenses, name="report_expenses"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from dictionary.models import Transport, Employee
-from finance_operations.models import Expense, Income
+from finance_operations.models import Expense, Income, WorkSheet
+from common.models import ExpenseType
 from django.db.models import Sum
 
 # Create your views here.
@@ -68,5 +69,97 @@ def index(request):
     return render(request, "dashboard/dashboard.html", context={
         'transports': transports,
         'employees': employees,
+        'total': total
+    })
+
+def report(request):
+    transports = Transport.objects.filter()
+    employees = Employee.objects.all()
+
+
+    total_expenses_summ = 0
+    total_incomes_summ = 0
+
+
+    total_expenses = Expense.objects.filter().aggregate(expenses_summ=Sum('summ'))
+    total_incomes = Income.objects.filter().aggregate(incomes_summ=Sum('summ'))
+    total_expenses_summ += total_expenses['expenses_summ'] or 0
+    total_incomes_summ += total_incomes['incomes_summ'] or 0
+
+    total = {
+        'expenses_summ': total_expenses_summ,
+        'incomes_summ': total_incomes_summ,
+        'benefit': total_incomes_summ - total_expenses_summ
+    }
+
+    print(total)
+
+
+    worksheets = WorkSheet.objects.filter()
+
+    for worksheet in worksheets:
+
+        expenses_summ = 0
+        incomes_summ = 0
+
+        expenses = worksheet.expense_set.all().aggregate(expenses_summ=Sum('summ'))
+        incomes = worksheet.income_set.all().aggregate(incomes_summ=Sum('summ'))
+        expenses_summ += expenses['expenses_summ'] or 0
+        incomes_summ += incomes['incomes_summ'] or 0
+        worksheet.expenses_summ = expenses_summ
+        worksheet.incomes_summ = incomes_summ
+        worksheet.benefit_summ = incomes_summ - expenses_summ
+ 
+
+
+    return render(request, "dashboard/report.html", context={
+        'transports': transports,
+        'employees': employees,
+        'worksheets': worksheets,
+        'total': total
+    })
+
+
+def report_expenses(request):
+    transports = Transport.objects.filter()
+    employees = Employee.objects.all()
+    expense_types = ExpenseType.objects.all()
+
+
+    total_expenses_summ = 0
+    total_incomes_summ = 0
+
+
+    total_expenses = Expense.objects.filter().aggregate(expenses_summ=Sum('summ'))
+    total_incomes = Income.objects.filter().aggregate(incomes_summ=Sum('summ'))
+    total_expenses_summ += total_expenses['expenses_summ'] or 0
+    total_incomes_summ += total_incomes['incomes_summ'] or 0
+
+    total = {
+        'expenses_summ': total_expenses_summ,
+        'incomes_summ': total_incomes_summ,
+        'benefit': total_incomes_summ - total_expenses_summ
+    }
+
+    print(total)
+
+
+    expenses = Expense.objects.all()
+
+    for expense in expenses:
+
+        expenses_summ = 0
+
+        expense_s =  expenses.aggregate(expenses_summ=Sum('summ'))
+        expenses_summ = expense_s['expenses_summ'] or 0
+        expense.expenses_summ = expenses_summ
+ 
+
+
+    return render(request, "dashboard/report_expenses.html", context={
+        'transports': transports,
+        'employees': employees,
+        'expenses': expenses,
+        'expense_types': expense_types,
         'total': total
     })
